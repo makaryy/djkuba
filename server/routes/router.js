4d88103ecdd9b7086c3f4e5d0f8807c9c99a855d
unknown <makaryyrakam@gmail.com>
2021-12-12 10:57:38 +0100

    delete firebase on server

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,11 +1,9 @@
 import express from "express";
 
 import { sendMail } from "../controllers/mail.js";
-import { getGallery } from "../controllers/gallery.js";
 
 const router = express.Router();
 
 router.post("/send", sendMail);
-router.get("/gallery", getGallery);
 
 export default router;
